84fef071db616d63036a1f7a796169b6f3d79720
manuel.gramazio <Manuel.Gramazio@camao.one>
2023-05-10 10:30:07 +0200

    added setup script and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # setup-workstation
-Sets the workstation for the trainee up
+Sets the workstation for the trainee up.
+
+## run
+### cloning the repository
+```cmd
+git clone https://github.com/trainee-tasks/setup-workstation.git
+.\setup-workstation\setup.ps1
+```
+
+### running directly the script without cloning repository
+```
+Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/trainee-tasks/setup-workstation/main/setup.ps1").Content
+```
---
+++ setup.ps1
@@ -0,0 +1,57 @@
+# Install Winget (if not already installed) with the latest version
+if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Installing Winget..."
+    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\winget.msixbundle"
+    Add-AppPackage -Path "$env:TEMP\winget.msixbundle" -ForceApplicationShutdown
+    Write-Host "Winget installed successfully!"
+}
+else {
+    Write-Host "Winget is already installed."
+}
+
+# Install Visual Studio Code using Winget
+Write-Host "Installing Visual Studio Code..."
+winget install --exact --id Microsoft.VisualStudioCode
+Write-Host "Visual Studio Code installed successfully!"
+
+Write-Host "Installing Git..."
+winget install --exact --id Git.Git
+Write-Host "Git installed successfully!"
+
+# Array of extensions to install
+$extensions = @(
+    "ritwickdey.liveserver"
+)
+
+# Install each extension using the VSCode command-line interface
+$vscode_path = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Where-Object { $_.DisplayName -eq "Visual Studio Code" }).InstallLocation
+$vscode_exe = Join-Path $vscode_path "bin\code.cmd"
+
+foreach ($extension in $extensions) {
+    & $vscode_exe --install-extension $extension
+}
+
+# Define the folder to clone the repositories into
+$destination_folder = "C:\repos"
+
+# Define an array of repository URLs to clone
+$repository_urls = @(
+    "https://github.com/trainee-tasks/calculator-js.git"
+)
+
+# Clone each repository into the destination folder
+foreach ($url in $repository_urls) {
+    $folder_name = $url.Split("/")[-1].Replace(".git", "")
+    $folder_path = Join-Path $destination_folder $folder_name
+    if (Test-Path $folder_path) {
+        Write-Host "Folder '$folder_path' already exists, skipping clone."
+    }
+    else {
+        Write-Host "Cloning repository '$url' into folder '$folder_path'."
+        git clone $url $folder_path
+    }
+}
+
+# Opens VS Code from the repos folder
+code "C:\repos"
